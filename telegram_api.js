e70fd0a086c76e8596f0b0c90cd56e67cb780dcb
sawerford10 <sawerford10@gmail.com>
2024-09-15 22:52:26 +0300

    telegram api function

---
+++ telegram_api.js
@@ -0,0 +1,32 @@
+import tokens from "./tokens.json" assert { type: "json" };
+import fs from "fs";
+import fetch from "node-fetch";
+import FormData from "form-data";
+
+const TOKEN = tokens.telegram_token;
+const CHAT_ID = tokens.telegram_chatId;
+const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+const photoUrl = `https://api.telegram.org/bot${TOKEN}/sendPhoto?chat_id=${CHAT_ID}`
+
+function createTextMessage(prediction){
+    const sendingBody = { "chat_id": CHAT_ID, "text": "hello world!" };
+    return JSON.stringify(sendingBody);
+}
+
+function createImageMessage(image){
+    let readStream = fs.createReadStream("./image.jpg");
+
+    let form = new FormData();
+    form.append("photo", readStream);
+    form.append("disable_notification", "true");
+    return { method: "POST", body: form }
+}
+
+export async function createTelegramPost(prediction, image) {
+    let res0 = await fetch(photoUrl, createImageMessage("./image.jpg"));
+    console.log(res0);
+    let res1 = await fetch(url, { method: "POST", body: createTextMessage(prediction), headers: { "Content-Type": "application/json" } });
+    console.log(res1);
+}
+
+createTelegramPost()
