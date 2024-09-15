87f49e45b099ca0deb00840ea2cff08bd563cd3b
sawerford10 <sawerford10@gmail.com>
2024-09-15 22:51:56 +0300

    shuffle function

---
+++ getCards.js
@@ -0,0 +1,22 @@
+import taro from "./taro.json" assert { type: "json" };
+
+function shuffle(array) {
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+}
+
+export function getCards() {
+    let cards = taro.cards;
+    shuffle(cards);
+    let layout = [];
+    for (let i = 0; i < 6; i++) {
+        layout.push(cards[i]);
+    }
+    return layout;
+}
